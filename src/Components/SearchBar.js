7a472bb08bb769a11a84bbb491984e9a853772f9
Adaś <adampekul@gmail.com>
2023-11-06 13:28:15 +0100

    install react-date-range, momento.js and setup date range in search component

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,12 +1,49 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
+import { DateRange } from "react-date-range";
+import { addDays } from "date-fns";
+import moment from "moment/moment";
+// Icons
 import { MdOutlineTravelExplore } from "react-icons/md";
 import { RiCalendar2Line } from "react-icons/ri";
 import { BsFillPersonFill } from "react-icons/bs";
 
 const SearchBar = () => {
+  // Date Ref
+  const dateRef = useRef();
+
+  // Destination
   const [place, setPlace] = useState("");
+
+  // Show Date Range
+  const [showDatePicker, setShowDatePicker] = useState(false);
+
+  // Date Range
+  const [date, setDate] = useState([
+    {
+      startDate: new Date(),
+      endDate: addDays(new Date(), 7),
+      key: "selection",
+    },
+  ]);
+
+  // Close Date picker menu if clicked outside
+  useEffect(() => {
+    const handleClick = (event) => {
+      if (dateRef.current && !dateRef.current.contains(event.target)) {
+        setShowDatePicker(false);
+      }
+    };
+
+    document.addEventListener("click", handleClick, true);
+
+    return () => {
+      document.removeEventListener("click", handleClick, true);
+    };
+  }, [dateRef]);
+
   return (
     <div className="bg-orange-400 p-2 rounded-md flex gap-2">
+      {/* Destination */}
       <ul className="w-full text-zinc-600 grid grid-cols-3 gap-2">
         <li className="relative">
           <input
@@ -18,15 +55,30 @@ const SearchBar = () => {
           <MdOutlineTravelExplore className="absolute top-2 left-2 text-2xl" />
         </li>
 
-        <li className="relative">
-          <input
-            type="date"
-            placeholder="Check-in date ---- Check-out date"
-            className="w-full px-10 py-2 rounded-md"
+        {/* date range Picker */}
+        <li className="relative" ref={dateRef}>
+          <div
+            className="w-full px-10 py-2 rounded-md bg-white cursor-pointer"
+            onClick={() => setShowDatePicker(!showDatePicker)}
+          >
+            {moment(date[0].startDate).format("ddd D MMM ")} -{" "}
+            {moment(date[0].endDate).format("ddd D MMM")}
+            <RiCalendar2Line className="absolute top-2 left-2 text-2xl" />
+          </div>
+          <DateRange
+            className={`absolute top-12 shadow-md transition-all ${
+              showDatePicker ? "visible" : "invisible"
+            }`}
+            onChange={(item) => setDate([item.selection])}
+            showSelectionPreview={true}
+            moveRangeOnFirstSelection={false}
+            months={2}
+            ranges={date}
+            direction="horizontal"
           />
-          <RiCalendar2Line className="absolute top-2 left-2 text-2xl" />
         </li>
 
+        {/* How many persons */}
         <li className="relative">
           <input
             placeholder="2 adults - 0 children - 1 room"
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// react-date-range
+import "react-date-range/dist/styles.css"; // main css file
+import "react-date-range/dist/theme/default.css"; // theme css file
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
