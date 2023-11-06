24407578d6efec9f360193c75c8bfbe646ff0198
Adaś <adampekul@gmail.com>
2023-11-06 16:06:37 +0100

    add dropdown menu of person value  and functionality

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -6,18 +6,22 @@ import moment from "moment/moment";
 import { MdOutlineTravelExplore } from "react-icons/md";
 import { RiCalendar2Line } from "react-icons/ri";
 import { BsFillPersonFill } from "react-icons/bs";
+import { IoMdArrowDropdown, IoMdArrowDropup } from "react-icons/io";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 
 const SearchBar = () => {
-  // Date Ref
   const dateRef = useRef();
+  const personRef = useRef();
 
   // Destination
   const [place, setPlace] = useState("");
 
-  // Show Date Range
-  const [showDatePicker, setShowDatePicker] = useState(false);
+  // Persons
+  const [showPerson, setShowPerson] = useState(false);
+  const [person, setPerson] = useState({ adults: 1, children: 0, room: 1 });
 
   // Date Range
+  const [showDatePicker, setShowDatePicker] = useState(false);
   const [date, setDate] = useState([
     {
       startDate: new Date(),
@@ -26,11 +30,27 @@ const SearchBar = () => {
     },
   ]);
 
-  // Close Date picker menu if clicked outside
+  // Increment/Decrement Person
+  const IncrementPerson = (key) => {
+    setPerson((prev) => ({ ...prev, [key]: prev[key] + 1 }));
+  };
+  const DecrementPerson = (key) => {
+    if (key === "adults" || key === "room") {
+      if (person[key] === 1) return;
+      return setPerson((prev) => ({ ...prev, [key]: prev[key] - 1 }));
+    }
+    if (person[key] <= 0) return;
+    setPerson((prev) => ({ ...prev, [key]: prev[key] - 1 }));
+  };
+
+  // Close DropDown Menu if cilcked outside of its container
   useEffect(() => {
     const handleClick = (event) => {
       if (dateRef.current && !dateRef.current.contains(event.target)) {
         setShowDatePicker(false);
+      }
+      if (personRef.current && !personRef.current.contains(event.target)) {
+        setShowPerson(false);
       }
     };
 
@@ -39,7 +59,7 @@ const SearchBar = () => {
     return () => {
       document.removeEventListener("click", handleClick, true);
     };
-  }, [dateRef]);
+  }, [dateRef, personRef]);
 
   return (
     <div className="bg-orange-400 p-2 rounded-md flex gap-2">
@@ -65,26 +85,81 @@ const SearchBar = () => {
             {moment(date[0].endDate).format("ddd D MMM")}
             <RiCalendar2Line className="absolute top-2 left-2 text-2xl" />
           </div>
-          <DateRange
-            className={`absolute top-12 shadow-md transition-all ${
-              showDatePicker ? "visible" : "invisible"
-            }`}
-            onChange={(item) => setDate([item.selection])}
-            showSelectionPreview={true}
-            moveRangeOnFirstSelection={false}
-            months={2}
-            ranges={date}
-            direction="horizontal"
-          />
+          {showDatePicker && (
+            <DateRange
+              className="absolute top-12 shadow-md transition-all"
+              onChange={(item) => setDate([item.selection])}
+              showSelectionPreview={true}
+              moveRangeOnFirstSelection={false}
+              months={2}
+              ranges={date}
+              direction="horizontal"
+            />
+          )}
         </li>
 
         {/* How many persons */}
-        <li className="relative">
-          <input
-            placeholder="2 adults - 0 children - 1 room"
-            className="w-full px-10 py-2 rounded-md"
-          />
-          <BsFillPersonFill className="absolute top-2 left-2 text-2xl" />
+        <li className="relative select-none" ref={personRef}>
+          <div
+            className="w-full px-10 py-2 rounded-md bg-white cursor-pointer flex gap-2"
+            onClick={() => setShowPerson(!showPerson)}
+          >
+            <p>
+              {person.adults} adults · {person.children} children ·{" "}
+              {person.room} room
+            </p>
+            <BsFillPersonFill className="absolute top-2 left-2 text-2xl" />
+            {showPerson ? (
+              <IoMdArrowDropup className="absolute top-2 right-2 text-2xl" />
+            ) : (
+              <IoMdArrowDropdown className="absolute top-2 right-2 text-2xl" />
+            )}
+          </div>
+          {showPerson && (
+            <ul className="absolute bg-white top-12 shadow-md transition-all w-full flex flex-col items-center">
+              {Object.keys(person).map((v, i) => (
+                <li
+                  key={i}
+                  className="flex w-full items-center justify-between px-10 h-16"
+                >
+                  <span>{v}</span>
+                  <div className="flex gap-2 items-center font-bold border border-black">
+                    <button
+                      onClick={() => DecrementPerson(v)}
+                      className={`flex items-center justify-center text-2xl p-2 transition-all hover:bg-blue-100
+                      ${
+                        person[v] <= 1 &&
+                        v !== "children" &&
+                        "cursor-not-allowed text-gray-300 hover:bg-white"
+                      }
+                      ${
+                        person[v] <= 0 &&
+                        v === "children" &&
+                        "cursor-not-allowed text-gray-300 hover:bg-white"
+                      }`}
+                    >
+                      <AiOutlineMinus />
+                    </button>
+                    <span className="w-5 flex justify-center">{person[v]}</span>
+                    <button
+                      onClick={() => IncrementPerson(v)}
+                      className="flex items-center justify-center text-2xl p-2 transition-all hover:bg-blue-100"
+                    >
+                      <AiOutlinePlus />
+                    </button>
+                  </div>
+                </li>
+              ))}
+              <li className="w-full">
+                <button
+                  onClick={() => setShowPerson(false)}
+                  className="bg-indigo-500 px-2 py-1 w-full rounded-sm font-bold text-white transition-all hover:bg-indigo-800"
+                >
+                  Done
+                </button>
+              </li>
+            </ul>
+          )}
         </li>
       </ul>
 
